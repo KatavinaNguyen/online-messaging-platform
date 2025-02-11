60200f1678b47ddb24d6181baa737e3cd71c3c23
KatavinaNguyen <knguyen1.ga@gmail.com>
2025-02-11 01:05:23 -0500

    created spring boot project

---
+++ src/test/java/com/example/online_messaging/OnlineMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
